b4e6cc8c08a6a0bc7ab24bd49031aa284450030f
Josh Wilson <josh.wilson@fivestars.com>
2016-10-01 11:25:35 -0700

    Cleanup

--- bash/dr.sh
+++ bash/dr.sh
@@ -36,11 +36,36 @@ Commands:
 
     Open a bash shell on the given <service>, or run <command>, if given.
 
-  dr create-node <node> [-s] [-m <swarm>] [-w <swarm>] [i <ip address>]
+  dr create-node <node> [-s] [-m <swarm>] [-w <swarm>]
+                        [-i <ip address>]
+                        [-u <user id>:<group id>]
 
-    Create a new docker machine (using virtualbox) and optionally create a new swarm with the node
-    as a manager or join the new node to an existing swarm.
+    Create a new docker machine (using virtualbox) and optionally create a new
+    swarm with the node as a manager or join the new node to an existing swarm.
 
+    If -i <ip address> is provided, the new node will be configured to use it as
+    it's static IP address. Otherwise, it will use whatever address was
+    provided by the VirtualBox DHCP service. Your /etc/hosts file will be
+    updated with an entry for this new node.
+
+    Your ~/ directory will be exported to and mounted by the new node. Your
+    /etc/exports file (and /etc/nfs/server.map file) will be updated
+    accordingly. For Cygwin users only: if -u <user id>:<group id> is provided
+    the server.map file will use those values rather than 0:0 (root) for the
+    user/group mapping. The files in the NFS mount will appear to be owned by
+    the node's user and group corresponding to the given ids.
+
+    Create standard non-swarm node:
+        dr create-node mynode
+
+    Create a manager node for a new swarm:
+        dr create-node myswarmmanager -s
+
+    Create a manager node for an existing swarm:
+        dr create-node mynewmanager -m myswarmmanager
+
+    Create a worker node for an existing swarm:
+        dr create-node mynewworker -w myswarmmanager
 
 EOF
 }
@@ -61,8 +86,10 @@ function dr-env {
 }
 
 function dr-logs {
-    local swarm=$(docker info | grep -q "Swarm: active" && echo true || echo false)
-    local manager=$($swarm && docker info | grep -q "Is Manager: true" && echo true || echo false)
+    local swarm=$(docker info | grep -q "Swarm: active" &&
+                  echo true || echo false)
+    local manager=$($swarm && docker info | grep -q "Is Manager: true" &&
+                    echo true || echo false)
     local service container name node
     local choice
 
@@ -86,24 +113,34 @@ function dr-logs {
                     docker ps --format "{{.Names}}" | grep "^$name"
                 )
 
+                # Output the logs, and prefix each line with its
+                # container designation.
                 (
                     eval $(docker-machine env $node)
-                    docker logs ${@:1:$(( $# - 1 ))} $container |& sed "s/^/${container%.*}: /g"
+                    docker logs ${@:1:$(( $# - 1 ))} $container |&
+                        sed "s/^/${container%.*}: /g"
                 ) &
-            done < <(docker service ps $service | tail -n +2 | grep "Running" | grep -v "\\_" | awk '{ print $2" "$4 }')
+            done < <(docker service ps $service | tail -n +2 | grep "Running" |
+                grep -v "\\_" | awk '{ print $2" "$4 }')
             wait
         else
-            # The argument could be a single instance of the service (eg. service.2) or just a raw
-            # container on the node itself (eg. service.2.e0w8b8t2fa0vtyl4yxs3d2qu7).
+            # The argument could be a single instance of the service (service.2)
+            # or just a raw container on the node itself
+            # (service.2.e0w8b8t2fa0vtyl4yxs3d2qu7).
             container=$service
             name=${container%.*}
             service=${container%%.*}
             if docker service ps $service &>/dev/null; then
                 # They are trying to get logs for an instance of a service
-                node=$(docker service ps $service | tail -n +2 | grep $name | awk '{ print $4 }')
-                ( eval $(docker-machine env $node); docker logs ${@:1:$(( $# - 1 ))} $container )
+                node=$(docker service ps $service | tail -n +2 | grep $name |
+                    awk '{ print $4 }')
+                (
+                    eval $(docker-machine env $node)
+                    docker logs ${@:1:$(( $# - 1 ))} $container
+                )
             else
-                # They are trying to get logs for a normal non-service container on the current node
+                # They are trying to get logs for a normal non-service
+                # container on the current node
                 docker logs $container
             fi
         fi
@@ -113,8 +150,10 @@ function dr-logs {
 }
 
 function dr-shell {
-    local swarm=$(docker info | grep -q "Swarm: active" && echo true || echo false)
-    local manager=$($swarm && docker info | grep -q "Is Manager: true" && echo true || echo false)
+    local swarm=$(docker info | grep -q "Swarm: active" &&
+                  echo true || echo false)
+    local manager=$($swarm && docker info | grep -q "Is Manager: true" &&
+                    echo true || echo false)
     local service container name node=$DOCKER_MACHINE_NAME
     local output choice
 
@@ -126,16 +165,17 @@ function dr-shell {
         [[ $choice == $service ]] && choice=
 
         if [[ "$choice" == "*" && $# -eq 0 ]]; then
-            printf "Cannot open a shell on \"*\". Specify a service instance.\n" >&2
+            printf "Cannot open a shell on \"*\"." >&2
+            printf " Specify a service instance.\n" >&2
             return 1
         fi
 
         if docker service ps $service &>/dev/null; then
             output=$(mktemp); trap "rm -f $output" EXIT
-            docker service ps $service | \
-                tail -n +2 | \
-                grep "Running" | \
-                grep -v "\\_" | \
+            docker service ps $service |
+                tail -n +2 |
+                grep "Running" |
+                grep -v "\\_" |
                 awk '{ print $2" "$4 }' > $output
 
             if [[ $(wc -l <$output) -eq 1 ]]; then
@@ -150,9 +190,12 @@ function dr-shell {
                             )
                             (
                                 eval $(docker-machine env $node)
-                                docker exec $container ${@:1:$#} |& sed "s/^/${container%.*}: /g"
+                                docker exec $container ${@:1:$#} |&
+                                    sed "s/^/${container%.*}: /g"
                             ) &
-                        done < <(docker service ps $service | tail -n +2 | grep "Running" | grep -v "\\_" | awk '{ print $2" "$4 }')
+                        done < <(docker service ps $service | tail -n +2 |
+                                 grep "Running" | grep -v "\\_" |
+                                 awk '{ print $2" "$4 }')
                         wait
                         return
                     else
@@ -163,32 +206,38 @@ function dr-shell {
                     printf "%s\n" "------------------------------------"
                     (
                         eval $(docker-machine env loyalty)
-                        docker ps --format "table {{.Names}}\t{{.Status}}" | \
-                            tail -n +2 | \
-                            grep "$service\." | \
-                            sort | \
+                        docker ps --format "table {{.Names}}\t{{.Status}}" |
+                            tail -n +2 |
+                            grep "$service\." |
+                            sort |
                             awk '{print NR ") " $0}'
                     )
                     printf "Choose (default=1): "
                     read choice
-                    read name node < <(cat $output | tail -n +${choice:-1} | head -n 1)
+                    read name node < <(cat $output | tail -n +${choice:-1} |
+                                       head -n 1)
                 fi
                 if [[ -z $name ]];then
                     printf "Invalid choice\n" >&2
                     return 1
                 fi
             fi
-            container=$(eval $(docker-machine env $node); docker ps --format "{{.Names}}" | grep "^$name")
+            container=$(eval $(docker-machine env $node)
+                        docker ps --format "{{.Names}}" | grep "^$name")
         else
             container=$service
             name=${container%.*}
             service=${container%%.*}
 
             if docker service ps $service &>/dev/null; then
-                node=$(docker service ps $service | tail -n +2 | grep $name | awk '{ print $4 }')
+                node=$(docker service ps $service |
+                       tail -n +2 | grep $name | awk '{ print $4 }')
             fi
         fi
-        ( eval $(docker-machine env $node); docker exec -it $container ${*:-/bin/bash})
+        (
+            eval $(docker-machine env $node)
+            docker exec -it $container ${*:-/bin/bash}
+        )
     else
         docker exec -it $1 ${*:-/usr/bin/env bash}
     fi
@@ -211,8 +260,9 @@ function dr-boot {
             dr-env $manager
         fi
     else
-        printf "Create a ~/.dr file with a list of machines to start on boot up.\nFormat:\n" >&2
-        printf "DR_MACHINES=(docker-machine-1 docker-machine-2 ...)" >&2
+        printf "Create a ~/.dr file.\n" >&2
+        printf "DR_MACHINES: a list of machines to start on boot up.\n" >&2
+        printf "\tDR_MACHINES=(docker-machine-1 docker-machine-2 ...)\n" >&2
         return 1
     fi
 }
@@ -249,10 +299,10 @@ function dr-create-node {
         return 1
     fi
 
-    if docker-machine inspect $node &>/dev/null; then
-        printf "Node '$node' already exists\n" >&2
-        return 1
-    fi
+    # if docker-machine inspect $node &>/dev/null; then
+    #     printf "Node '$node' already exists\n" >&2
+    #     return 1
+    # fi
 
     if [[ -n $swarm ]]; then
         if ! docker-machine inspect $swarm &>/dev/null; then
@@ -260,7 +310,8 @@ function dr-create-node {
             return 1
         fi
 
-        if ! ( eval $(docker-machine env $swarm); docker info | grep -q "Is Manager: true" ); then
+        if ! ( eval $(docker-machine env $swarm)
+               docker info | grep -q "Is Manager: true" ); then
             printf "Node '$swarm' is not a swarm manager\n"
             return 1
         fi
@@ -277,7 +328,7 @@ function dr-create-node {
 
     printf "\nSharing your ~/ directory with the '%s'\n" $node
     printf "%s\n" "----------------------------------------"
-    docker-machine-nfs.sh $node -s=$HOME:/home/fivestars
+    docker-machine-nfs.sh $node -s=$HOME
 
     if $as_manager; then
         if [[ -z $swarm ]]; then
@@ -285,38 +336,37 @@ function dr-create-node {
             printf "\nInitializing swarm with '%s' as a manager\n" $node
             printf "%s\n" "----------------------------------------"
             docker-machine ssh $node docker swarm init --advertise-addr \
-                $(docker-machine ls | \
-                    grep -w $node | \
-                    awk '{ print $5 }' | \
+                $(docker-machine ls |
+                    grep -w $node |
+                    awk '{ print $5 }' |
                     sed 's/tcp:\/\/\(.*\):.*/\1/')
 
         else
             printf "\nJoining '%s' to swarm '%s' as a manager\n" $node $swarm
             printf "%s\n" "----------------------------------------"
             docker-machine ssh $node \
-                "$(docker-machine ssh $swarm docker swarm join-token manager | tail -n +2)"
+                "$(docker-machine ssh $swarm docker swarm join-token manager |
+                    tail -n +2)"
         fi
-
-        printf "\nSwarm nodes\n"
-        printf "%s\n" "----------------------------------------"
-        eval $(docker-machine env $swarm)
-        docker node ls
 
     elif $as_worker; then
         printf "\nJoining '%s' to swarm '%s' as a worker\n" $node $swarm
         printf "%s\n" "----------------------------------------"
         docker-machine ssh $node \
-            "$(docker-machine ssh $swarm docker swarm join-token worker | tail -n +2)"
-
-        printf "\nSwarm nodes\n"
-        printf "%s\n" "----------------------------------------"
-        (
-            eval $(docker-machine env $swarm)
-            docker node ls
-        )
+            "$(docker-machine ssh $swarm docker swarm join-token worker |
+                tail -n +2)"
     fi
 
-    printf "\Finished creating node '%s'\n" $node
+    if [[ -n $swarm ]]; then
+        printf "\nSwarm nodes\n"
+        printf "%s\n" "----------------------------------------"
+        eval $(docker-machine env $swarm)
+        docker node ls
+    fi
+
+    printf "\n%s\n" "----------------------------------------"
+    printf "Finished creating node '%s'\n" $node
+    printf "%s\n" "----------------------------------------"
 }
 
 function dr {
@@ -324,10 +374,12 @@ function dr {
     local cmd=${1:-help}
     shift
 
-    if [[ "$cmd" == "boot" ]]; then
-        dr-boot
+    if [[ "$cmd" == "boot" || "$cmd" == "create-node" ]]; then
+        # These commands should run directly in the current shell's environment
+        dr-$cmd $*
     elif command -v dr-$cmd &>/dev/null; then
-        # Invoke the appropriate function in a subshell (so bg jobs die with the function call)
+        # Invoke the appropriate function in a subshell
+        # (so bg jobs die with the function call)
         ( dr-$cmd $* )
     elif docker-machine env $cmd |& grep -qv "Host does not exist"; then
         # They are trying to use the dr-env shorthand?
