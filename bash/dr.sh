c24ae05afff8d96cc84a11c527614469e4a50660
Josh Wilson <josh.wilson@fivestars.com>
2016-10-25 15:09:31 -0700

    Really fix mktemp for OS X

--- bash/dr.sh
+++ bash/dr.sh
@@ -275,7 +275,7 @@ function dr-shell {
         fi
 
         if docker service ps $service &>/dev/null; then
-            output=$(mktemp); trap "rm -f $output" EXIT
+            output=$(mktemp -t dr-shell.XXXX); trap "rm -f $output" EXIT
             docker service ps $service |
                 tail -n +2 |
                 grep "Running" |
