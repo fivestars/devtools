e331bc609ac114616682f010e42226d5de5d8f12
Abhinav Gianey <abhinav.gianey@fivestars.com>
2017-01-25 16:35:46 -0800

    Made script output look better

--- k8s/hello-world-app/minikube-hello-world-app.sh
+++ k8s/hello-world-app/minikube-hello-world-app.sh
@@ -9,71 +9,61 @@ cmd=${1:-start}
 
 case $cmd in
     start)
-		PS3="Select your OS [Mac/Windows]: "
-		options=("Mac" "Windows")
-		select opt in "${options[@]}"; do
-		    case $opt in
-		        "${options[0]}") OS="$opt"; break ;;
-		        "${options[1]}") OS="$opt"; break ;;
-		        *) printf "Choose a valid option $opt\n" ;;
-		    esac
-		done
 		timestamp
-		echo "Starting Minikube... (~75 seconds)"
+		echo "Starting Minikube if not already started... (~75 seconds)"
 		if ! (  minikube status | grep Running > /dev/null ); then
 			minikube start  &>/dev/null
 		fi
 		eval $(minikube docker-env) &>/dev/null
 		echo Minikube Started
+		echo
 		timestamp
-		echo "Building hello-world-app docker image... (~ 90 seconds)"
+		echo "Building hello-world-app docker image if not already built... (~ 90 seconds)"
 		if ! [[ $(docker images -q hello-world-app:dev) ]]; then
 			docker build -t hello-world-app:dev . &>/dev/null
 		fi
 		echo Docker Image Built
+		echo
 		timestamp
 		echo "Creating Kubernetes Persistent Volume and Claim objects... (fast)"
-		if [[ $OS == "Mac" ]]; then
-			sed -e "s;%PATH%;$(pwd);g" pv.yaml.template > pv.yaml
-			kubectl create -f pv.yaml
-		elif [[ $OS == "Windows" ]]; then
-			kubectl create -f pv-windows.yaml
-		else 
-			echo Error in your OS choice 
-			exit 1
-		fi
+		sed -e "s;%PATH%;$(pwd);g" pv.yaml.template > pv.yaml
+		kubectl create -f pv.yaml
 		kubectl create -f pvc.yaml
+		echo
 		timestamp
 		echo "Creating hello-world-app deploy... (fast)"
 		kubectl create --validate=false -f deploymentConfig.yaml
+		echo
 		timestamp
 		echo "Exposing hello-world-app service... (fast)"
 		kubectl expose deployment hello-world-app --name=hello-world-app --type=NodePort
+		echo
 		timestamp
 		echo Sleeping for 10 seconds before checking service is up and running...
-		sleep 10 # Let service get up and running before curling
+		sleep 0 # Let service get up and running before curling
+		echo
 		endpoint=$(minikube ip)$(kubectl get svc | grep hello-world-app | egrep -o ':\d{5}')
 		echo Curling ${endpoint}, which is where your hello world app is. 
-		echo Expecting: 
-		echo Hello World
-		echo Got:
+		echo "Expecting:" 
+		echo "Hello World"
+		echo "Got:"
 		curl ${endpoint}
 		echo 
 		timestamp
 		;;
 	stop)
-		timestamp
 		echo Deleting hello-world-app deploy if it is there...
 		kubectl delete deploy hello-world-app &>/dev/null
 		echo Deleted hello-world-app deploy
+		echo
 		echo Deleting hello-world-app service if it is there...
 		kubectl delete svc hello-world-app &>/dev/null
 		echo Deleted hello-world-app service
+		echo
 		echo Deleting hello-world-app persistent volume and claim objects if they are there...
 		kubectl delete pv hello-world-app-persistent-volume &>/dev/null
 		kubectl delete pvc hello-world-app-persistentclaim &>/dev/null
 		echo Deleted hello-world-app persistent volume and claim objects
-		timestamp
 		;;
 	*)
 		echo "Invalid command: $cmd" >&2;
